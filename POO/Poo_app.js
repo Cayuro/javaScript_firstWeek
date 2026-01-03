a6fa7700b86d623dc9119116edc5f6fb4530e5b9
Cayuro <Juan_gomez2000@hotmail.com>
2026-01-03 08:31:16 -0500

    POO

---
+++ POO/Poo_app.js
@@ -0,0 +1,70 @@
+class App {
+    constructor(descargas,puntuacion,peso){
+        this.descargas =  descargas;
+        this.puntuacion = puntuacion;
+        this.peso = peso;
+        this.iniciada = false;
+        this.instalada = false;
+    }
+    abrir(){
+        if(this.iniciada== false && this.instalada == true){
+            this.iniciada = true;
+            alert("Iniciando aplicación")
+        }
+    }
+    cerrar(){
+        if(this.iniciada == true && this.instalada == true){
+            this.iniciada = false;
+            alert("cerrando la app")
+        }
+    }
+    instalar(){
+        if(this.instalada == false){
+            this.instalada = true;
+            alert("la aplicación ha sido instalada correctamente")
+        }
+    }
+    desinstalar(){
+        if(this.instalada == true){
+            this.instalada = false;
+            alert("Aplicación desinstalada correctamente")
+        }
+    }
+    appInfo(){
+        return `<br>
+        <h3>Aplicación información: </h3>
+        Descargas: <b style="color: blue;"> ${this.descargas}</b> <br>
+        Puntuación: <b style="color: blue;"> ${this.puntuacion}</b> <br>
+        Peso: <b style="color: blue;"> ${this.peso}</b> <br>
+        `;
+    }
+}
+
+const app = new App("20", "4.5", "900mb");
+const app2 = new App("12.000", "4.2", "800mb");
+const app3 = new App("18.000", "4.7", "1gb");
+const app4 = new App("20.000", "4.8", "250mb");
+const app5 = new App("900", "5", "750mb");
+const app6 = new App("22.000", "4.9", "1.8gb");
+const app7 = new App("42.981", "2.9", "2.0gb");
+
+app.instalar();
+app.abrir();
+app.cerrar();
+app.desinstalar();
+
+// let array = [app2, app3, app4, app5, app6, app7];
+// for (let i = 0; i < array.length; i++){
+//     document.writeln(array[i].appInfo());
+//     array[i].instalar();
+//     array[i].abrir();
+//     array[i].cerrar();
+//     array[i].desinstalar();
+// }
+
+
+document.write(app.appInfo());
+
+for (let i = 2; i <=7; i++){
+    document.write(eval("app"+i+".appInfo()"));
+}
--- POO/classCelulares.js
+++ POO/classCelulares.js
@@ -1,10 +1,10 @@
 class celulares {
-    constructor(color, peso, rdp, rdc, ram, rdsistema, precio) {
+    constructor(color, peso, rdp, rdc, ram) {
         this.color = color;
         this.peso = peso;
         this.resolucionDePantalla = rdp;
         this.resolucionDeCamara = rdc;
-        this.ram = ram;
+        this.memoriaRam = ram;
         this.encendido = false;
     }
     botonEncendido() {
@@ -29,11 +29,63 @@ class celulares {
     grabarVideo(){
         alert(`Video grabado en resolución de: ${this.resolucionDeCamara}`);
     }
+    mostrarInfo(){
+        return `
+        Color: <b style="color: blue;"> ${this.color}</b> <br>
+        Peso: <b style="color: blue;"> ${this.peso}</b> <br>
+        Tamaño: <b style="color: blue;"> ${this.resolucionDePantalla}</b> <br>
+        Resolución de cámara: <b style="color: blue;"> ${this.resolucionDeCamara}</b> <br>
+        Memoria RAM: <b style="color: blue;"> ${this.memoriaRam}</b> <br>
+        `;
+    }
+}
+class celularAltaGama extends celulares {
+    constructor(color, peso, rdp, rdc, ram, rdce) {
+        super(color, peso, rdp, rdc, ram);
+        this.resolucionDeCamaraExtra = rdce;
+    }
+    grabarVideoLento(){
+        alert("estás grabando en cámara lenta");
+    }
+    reconocimientoFacial(){
+        alert("reconocimiento facial activado");
+    }
+    infoAltaGama(){
+        return this.mostrarInfo() + `Resolución de cámara extra: <b style="color: blue;"> ${this.resolucionDeCamaraExtra}</b> <br>`;
+    }
 }
 
-const iphone = new celulares("rojo", "150gr", "5'", "4K", "2GB");
+const iphone = new celularAltaGama("rojo", "150gr", "5'", "4K", "2GB", "FULL HD");
+const samsung = new celularAltaGama("negro", "155gr", "5.5'", "FULL HD", "3GB", "HD");
+const nokia = new celulares("blanco", "160gr", "4.7'", "HD", "1GB");
 
-iphone.botonEncendido();
-iphone.reiniciar();
-iphone.tomarFoto();
-iphone.grabarVideo();
+// iphone.botonEncendido();
+// iphone.reiniciar();
+// iphone.tomarFoto();
+// iphone.grabarVideo();
+
+// samsung.botonEncendido();
+// samsung.reiniciar();
+// samsung.tomarFoto();
+// samsung.grabarVideo();
+
+// nokia.botonEncendido();
+// nokia.reiniciar();
+// nokia.tomarFoto();
+// nokia.grabarVideo();
+
+// document.write(`
+// <h3>Características del Iphone</h3>
+// ${iphone.mostrarInfo()}
+// <h3>Características del Samsung</h3>
+// ${samsung.mostrarInfo()}
+// <h3>Características del Nokia</h3>
+// ${nokia.mostrarInfo()}
+// `);
+
+document.write(`
+<h3>Características del Iphone de alta gama</h3>
+${iphone.infoAltaGama()}
+<h3>Características del Samsung de alta gama</h3>
+${samsung.infoAltaGama()}
+`);
